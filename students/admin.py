0cbf76b9d1a09e43a5f7253b4ebc695e34619ff4
Ahmad El Mokahal <a.mokahal_99@hotmail.com>
2024-09-28 20:13:05 -0400

    Admin Interface

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+'''
+    Superuser credentials:
+        username: admin
+        password: admin123
+'''
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'enrollment_date')
